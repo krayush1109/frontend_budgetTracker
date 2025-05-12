ec93ae96efc1a1f3a294d065fa9ccfeb923c43b4
krayush1109 <hellotoayush@gmail.com>
2025-05-12 15:40:21 +0530

    AuthGuard Implemented

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -9,18 +9,21 @@ import { DashboardComponent } from './component/dashboard/dashboard.component';
 import { AddIncomeComponent } from './component/add-income/add-income.component';
 import { ExpensesComponent } from './component/expenses/expenses.component';
 
-
+import { AuthGuard } from './authGuard/auth.guard';
 
 export const routes: Routes = [
-    {path:'',component:HomeComponent },
-    {path:'login',component:LoginComponent},
-    {path:'signup',component:SignupComponent},
-
-    { path: 'add-income', component: AddIncomeComponent },
-    { path: 'expenses', component: ExpensesComponent },
-
-    { path: 'dashboard', component: DashboardComponent }
-    
-    
-    
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  {
+    path: 'add-income',
+    component: AddIncomeComponent,
+    canActivate: [AuthGuard],
+  },
+  { path: 'expenses', component: ExpensesComponent, canActivate: [AuthGuard] },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    canActivate: [AuthGuard],
+  },
 ];
